7ad95ee7eb06917a755f34c32136d2be92bd3bd6
Chinomiy <maruishiwo@gmail.com>
2023-10-15 19:47:52 +0300

    v1

--- src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
+++ src/main/java/school/mjc/stage0/conditions/finalTask/DaysInMonth.java
@@ -1,7 +1,15 @@
 package school.mjc.stage0.conditions.finalTask;
 
+import java.time.YearMonth;
+
 public class DaysInMonth {
     public void printDays(int year, int month) {
-
+        if (month > 12 || month <= 0) {
+            System.out.println("invalid date");
+            return;
+        }
+        YearMonth yearMonth = YearMonth.of(year,month);
+        int daysInMonth = yearMonth.lengthOfMonth();
+        System.out.println(daysInMonth);
     }
 }
